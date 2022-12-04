f85dc4afe7f78dbf1690bf69f78def91b321d055
Diego Auza <auza0002@algonquinlive.com>
2022-12-04 14:14:32 -0500

    done 100%

--- main.js
+++ main.js
@@ -4,17 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 import pickRandomName from "./data.js";
-import {NetworkError} from "./util.js";
+import { NetworkError } from "./util.js";
 
 const selector = document.querySelector("#category");
 const keyAPI = `live_B3MS8jrg6WiRwgWSEfC8seqWmvYtectC7NczZGtuBtmwBN5c0qahR9KVNz6QuDuk`;
 let url = `https://api.thecatapi.com/v1/categories`;
 let ul = document.querySelector(".content-grid");
-let divContainer = document.querySelector(".div_error")
+let divContainer = document.querySelector(".div_error");
 let spinImg = document.querySelector(".loader_img");
 let cache = getLocalStorage();
 
-  
 function catCategories(url) {
   selector.innerHTML = `<option>Categories</option>`;
   fetch(url)
@@ -33,31 +32,31 @@ function catCategories(url) {
       });
     })
     .catch((err) => {
-      return errorCategories(true, err)
+      return errorCategories(true, err);
     });
 }
 
 function getImgCats(ev) {
-if(ev.target.value === "Categories"){
-        return;
+  if (ev.target.value === "Categories") {
+    return;
   }
   url = `https://api.thecatapi.com/v1/images/search?limit=10&category_ids=${ev.target.value}&api_key${keyAPI}`;
   ul.innerHTML = ``;
   fetch(url)
     .then((response) => {
-      console.log(response)
-  if (!response.ok) {
-    spin(false)
-    throw new NetworkError('Failed API Call', response);
-      }else{
-        spin(true)
-      return delay(2000).then(() => response.json());
+      console.log(response);
+      if (!response.ok) {
+        spin(false);
+        throw new NetworkError("Failed API Call", response);
+      } else {
+        spin(true);
+        return delay(2000).then(() => response.json());
       }
-    }) 
+    })
     .then((data) => {
       spin(false);
-      ul.innerHTML = data.map(item => {
-          let catNames = pickRandomName();
+      ul.innerHTML = data
+        .map((item) => {
           if (item.id in cache) {
             return `
             <li>
@@ -69,7 +68,9 @@ if(ev.target.value === "Categories"){
             </li>
         `;
           } else {
-            cache[(item.id)] = `${catNames}`;
+            let catNames = pickRandomName();
+            cache[item.id] = `${catNames}`;
+            saveToLocalStorage();
             return `
             <li>
             <figure>
@@ -81,7 +82,6 @@ if(ev.target.value === "Categories"){
           }
         })
         .join("");
-         saveToLocalStorage();
     })
     .catch((err) => {
       return errorCategories(true, err);
@@ -89,43 +89,41 @@ if(ev.target.value === "Categories"){
 }
 
 function saveToLocalStorage() {
-  localStorage.setItem('cache-auza0002', JSON.stringify(cache));
+  localStorage.setItem("cache-auza0002", JSON.stringify(cache));
 }
-function getLocalStorage(){
-  let cache = localStorage.getItem("cache-auza0002")
+function getLocalStorage() {
+  let cache = localStorage.getItem("cache-auza0002");
   return cache ? JSON.parse(cache) : {};
 }
-function spin(show){
-  if (show === true){
-    spinImg.classList.add('active');
+function spin(show) {
+  if (show === true) {
+    spinImg.classList.add("active");
     errorCategories(false);
-  }else {
-    spinImg.classList.remove('active')
+  } else {
+    spinImg.classList.remove("active");
   }
 }
 function delay(time) {
   return new Promise((resolve, reject) => {
     setTimeout(resolve, time);
   });
-
 }
-function errorCategories(show, err){
-  if (show){
-    console.log(err.status)
+function errorCategories(show, err) {
+  if (show) {
+    console.log(err.status);
     let errorP = document.querySelector(".error_p");
-    if (err.status === 404){
-      errorP.innerHTML = `Error ${err.status}, try again. no images found`
+    if (err.status === 404) {
+      errorP.innerHTML = `Error ${err.status}, try again. no images found`;
       spin(false);
-      console.warn(err)
-      divContainer.classList.add('active');
+      console.warn(err);
+      divContainer.classList.add("active");
     } else {
       spin(false);
-      console.warn(err)
-      divContainer.classList.add('active');
-      return errorP.innerHTML = `there is something wrong, try again. double check your connection`
+      console.warn(err);
+      divContainer.classList.add("active");
+      return (errorP.innerHTML = `there is something wrong, try again. double check your connection`);
     }
-  }else {
-    divContainer.classList.remove('active');
+  } else {
+    divContainer.classList.remove("active");
   }
 }
-
